253e7e8750dd635b293857dd1dad1e1b70245f39
unknown <emmanuelnkubito2@gmail.com>
2020-09-01 23:32:34 +0200

    diplaying all blogs in collection

--- fetchUser.js
+++ fetchUser.js
@@ -5,29 +5,24 @@
     let password  = document.getElementById('Upass').value;
     let conf_password  = document.getElementById('Urep_pass').value;
     if(password == conf_password ){
-      fetch("http://localhost:2300/api/v1/user/new", { 
-        // Adding method type 
+      fetch("https://voicetoworld.herokuapp.com/api/v1/user/new", { 
         method: "POST", 
-        // Adding body or contents to send 
         body: JSON.stringify({ 
           email,
           password,
           name
         }), 
-        // Adding headers to the request 
         headers: { 
             "Content-type": "application/json; charset=UTF-8"
         } 
       }) 
-      // Converting to JSON 
       .then(response => response.json()) 
-      // Displaying results to console 
       .then(json => console.log(json));
       
              } 
-  else{
-    alert("unMarched Password");
-  }
+    else{
+      alert("Un-marched Password");
+    }
   }
   
   function signIn(e){
@@ -38,76 +33,64 @@
       email: email,
       password: password
     }
-    fetch(`http://127.0.0.1:2300/api/v1/login`, { 
-      // Adding method type 
+    fetch(`https://voicetoworld.herokuapp.com/api/v1/login`, { 
       method: "POST", 
-      // Adding body or contents to send 
       body: JSON.stringify({ 
         email,
         password
       }), 
-      // Adding headers to the request 
       headers: { 
           "Content-type": "application/json; charset=UTF-8"
       } 
     }) 
-    // Converting to JSON 
     .then(response => response.json()) 
-    // Displaying results to console 
     .then(json => console.log(json))
     .catch(err=>console.log('Login Error happen')); 
-  } 
-//Getting Blogs
-function getBlogPost(){
-  fetch('http://localhost:2300/api/v1/articles/')
-  .then ((res) => res.json())
-  .then ((data) =>{
-   // let output = '<centre><h2>Blogs</h2><centre>';
-    data.forEach(function(post){
-           `
-       <div class="row">
-      <div class="column">
-        <div class="card">
-          <h3>${post.title}</h3>
-          <p>${post.content}</p>
-          <b>
-          <p style="float: right;">created at ${post.date}</p>
-          </b>
-        </div>
-      </div>
-      </div>
-        `;
-    });
-   // document.getElementById('').innerHTML = output
-  })
-}
- 
+  }
+    
 function addPost(e){
   e.preventDefault();
   let title = document.getElementById('btitle').value;
   let author = document.getElementById('bimage').value;
   let content  = document.getElementById('bmsg').value;
-fetch("http://127.0.0.1:2300/api/v1/articles/new", { 
-  // Adding method type 
+fetch("https://voicetoworld.herokuapp.com/api/v1/article/new", { 
   method: "POST", 
-  // Adding body or contents to send 
   body: JSON.stringify({ 
     title,
     author,
     content
   }), 
-  // Adding headers to the request 
   headers: { 
       "Content-type": "application/json; charset=UTF-8"
   } 
 }) 
-// Converting to JSON 
 .then(response => response.json()) 
-// Displaying results to console 
-.then(json => console.log(json));
-} 
- 
+.then(json => console.log(json))
+.catch(err=>console.log('Adding Blog Error happen'));
+}
+
+function saveBlog(){
+  let title = document.getElementById('btitle').value;
+  let author = document.getElementById('bimage').value;
+  let content  = document.getElementById('bmsg').value;
+  if(title == "" || author == "" || content == ""){
+    alert("required fields can not be empty")
+  } else{ 
+  fetch(`https://voicetoworld.herokuapp.com/api/v1/article/new`, { 
+      method: "POST", 
+      body: JSON.stringify({ 
+       title,author,content
+      }), 
+      headers: { 
+        "Content-type": "application/json; charset=UTF-8"
+      } 
+    })  
+    .then(response => response.json()) 
+    .then(json => console.log(json))
+    .catch(err=>console.log('Adding Blog Error happen'));  
+  }
+}
     document.getElementById("signups").addEventListener("submit", addUser);
     document.getElementById("MyForm").addEventListener("submit", signIn);
-    document.getElementById("sub").addEventListener("click", addPost);
-    document.getElementById("blogs").addEventListener("load", getBlogPost);
+    //document.getElementById('bform').addEventListener('submit', saveBlog);
+    //document.getElementById("blogs").addEventListener("load", getBlogPost);
